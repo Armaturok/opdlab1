70574d4619d9411caade1e0d0c5eb59cdc052548
MaxGit <kusmvtov14@mail.ru>
2025-04-15 15:36:42 +0600

    Update p3.py

--- p3.py
+++ p3.py
@@ -1,15 +1,30 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://omgtu.ru/structure/departments"
+url = "https://omgtu.ru/general_information/the-structure/the-department-of-university.php"
 response = requests.get(url)
 
 if response.status_code == 200:
     soup = BeautifulSoup(response.text, 'html.parser')
-    departments = soup.find_all('div', class_='department-name')
 
+    # Ищем все элементы <div class="col-md-6">
+    department_blocks = soup.find_all('div', class_='main__content')
+
+    departments = []
+
+    for block in department_blocks:
+        # Внутри блока находим ссылки на кафедры
+        links = block.find_all('a')
+        for link in links:
+            name = link.get_text(strip=True)
+            if name:
+                departments.append(name)
+
+    # Записываем в файл
     with open('departments.txt', 'w', encoding='utf-8') as file:
         for dept in departments:
-            file.write(dept.get_text(strip=True) + '\n')
+            file.write(dept + '\n')
 
-print("Список кафедр был успешно записан в файл 'departments.txt'.")
+    print("Список кафедр был успешно записан в файл 'departments.txt'.")
+else:
+    print(f"Ошибка при подключении: {response.status_code}")
